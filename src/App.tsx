87edb40277534a890f05e0cc9e40e7fe222376b2
juan <judagosa8520@gmail.com>
2024-11-27 14:44:47 -0500

    feat: initialize React, TypeScript, and Vite setup with basic configuration and structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  )
+}
+
+export default App
